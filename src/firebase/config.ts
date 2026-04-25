ab27b7ab2d4e55797f6c4a21d50fbc4a4bea21a3
Andri Mahardika <andry.mhd@gmail.com>
2026-04-25 16:39:25 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6458030011-4ec47",
+  "appId": "1:843996871273:web:5741c2c317420af21600f8",
+  "apiKey": "AIzaSyA4mKV9KZiKqFsJv0gNQLbdOAzk09s1R_Q",
+  "authDomain": "studio-6458030011-4ec47.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "843996871273"
+};
